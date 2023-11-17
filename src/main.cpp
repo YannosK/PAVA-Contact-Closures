f24b2586cc1871840b09c2d62d78b54e379518fa
YannosK <giannos2405@gmail.com>
2023-11-17 11:38:28 +0200

    works

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,53 @@
 #include <Arduino.h>
 
-#define IN1 2
-#define IN2 3
+#define IN1 3
+#define IN2 2
 #define ROUT1 9
 #define ROUT2 7
 #define ROUT3 5
 #define BOUT 11
+#define LED 13
 
-volatile bool i1, i2;
+volatile int i1;
+volatile int i2;
 unsigned long previous_time;
 unsigned long current_time;
 
-void ISR_input1();
-void ISR_input2();
+void ISR_scada1();
+void ISR_scada2();
 
 void setup() {
-  pinMode(IN1, INPUT_PULLUP);
-  pinMode(IN2, INPUT_PULLUP);
+  pinMode(IN1, INPUT);
+  pinMode(IN2, INPUT);
   pinMode(ROUT1, OUTPUT);
   pinMode(ROUT2, OUTPUT);
   pinMode(ROUT3, OUTPUT);
   pinMode(BOUT, OUTPUT);
+  pinMode(LED, OUTPUT);
 
   digitalWrite(ROUT1, HIGH);
   digitalWrite(ROUT2, HIGH);
-  digitalWrite(ROUT3, HIGH);
+  digitalWrite(ROUT3, HIGH); 
 
-  if (digitalRead(IN1) == LOW)
-    i1 = true;
-  else
-    i1 = false;
-  if (digitalRead(IN2) == LOW)
-    i2 = true;
-  else
-    i2 = false;
+  while(digitalRead(IN1) == HIGH)
+    {}
+  i1 = LOW;
+  
+  while(digitalRead(IN2) == HIGH)
+    {}
+  i2 = LOW;
 
-  attachInterrupt(digitalPinToInterrupt(IN1), ISR_input1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(IN2), ISR_input2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(IN1), ISR_scada1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(IN2), ISR_scada2, CHANGE);
+
+  digitalWrite(LED, HIGH);
+  delay(250);
+  digitalWrite(LED, LOW);
+  delay(250);
+  digitalWrite(LED, HIGH);
+  delay(250);
+  digitalWrite(LED, LOW);
+  delay(250);
 }
 
 void loop() {
@@ -125,12 +136,18 @@ void loop() {
   digitalWrite(ROUT3, HIGH);
 }
 
-void ISR_input1()
+void ISR_scada1()
 {
-  i1 = !i1;
+  if (digitalRead(IN1) == HIGH)
+    i1 = HIGH;
+  else if (digitalRead(IN1) == LOW)
+    i1 = LOW;
 }
 
-void ISR_input2()
+void ISR_scada2()
 {
-  i2 = !i2;
+  if (digitalRead(IN2) == HIGH)
+    i2 = HIGH;
+  else if (digitalRead(IN2) == LOW)
+    i2 = LOW;
 }
