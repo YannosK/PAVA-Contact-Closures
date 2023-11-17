8649e95cba13dbb4e592af501f1a1e6e2ef3d9b6
YannosK <giannos2405@gmail.com>
2023-11-17 09:44:38 +0200

    Correcting errors

--- src/main.cpp
+++ src/main.cpp
@@ -36,89 +36,12 @@ void setup() {
 }
 
 void loop() {
-  while (i1 || i2)
-  {
-    while(i1)
-    {      
-      digitalWrite(ROUT2, HIGH);
-      digitalWrite(ROUT3, HIGH);
-      digitalWrite(ROUT1, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 1000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-      
-      digitalWrite(ROUT1, HIGH);
-      digitalWrite(ROUT3, HIGH);
-      digitalWrite(ROUT2, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 2000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-
-      digitalWrite(ROUT2, HIGH);
-      digitalWrite(ROUT1, HIGH);
-      digitalWrite(ROUT3, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 3000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-    }
-    while(i2)
-    {      
-      digitalWrite(ROUT2, HIGH);
-      digitalWrite(ROUT1, HIGH);
-      digitalWrite(ROUT3, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 3000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-
-      digitalWrite(ROUT1, HIGH);
-      digitalWrite(ROUT3, HIGH);
-      digitalWrite(ROUT2, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 2000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-
-      digitalWrite(ROUT2, HIGH);
-      digitalWrite(ROUT3, HIGH);
-      digitalWrite(ROUT1, LOW);
-      current_time = millis();
-      previous_time = current_time;
-      while ((current_time - previous_time) <= 1000)
-      {
-        current_time = millis();
-        if (!(i1 || i2))
-          goto endLoop;
-      }
-    }
-  }
-  endLoop:
-  digitalWrite(ROUT1, HIGH);
-  digitalWrite(ROUT2, HIGH);
-  digitalWrite(ROUT3, HIGH);
+  if (i1)
+    digitalWrite(ROUT1, LOW);
+  else if (i2)
+    digitalWrite(ROUT2, LOW);
+  else
+    digitalWrite(ROUT3, LOW);
 }
 
 void ISR_input1()
